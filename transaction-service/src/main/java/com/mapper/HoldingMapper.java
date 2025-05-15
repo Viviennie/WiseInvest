883d88b22d4d6184ebc52b2e9a46b7d5605ed3ed
yan-wen-qing <143249031+yan-wen-qing@users.noreply.github.com>
2025-05-15 21:53:18 +0800

    backend_transaction

---
+++ transaction-service/src/main/java/com/mapper/HoldingMapper.java
@@ -0,0 +1,9 @@
+package com.github.JLQusername.transaction.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.github.JLQusername.transaction.domain.Holding;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface HoldingMapper extends BaseMapper<Holding> {
+}
